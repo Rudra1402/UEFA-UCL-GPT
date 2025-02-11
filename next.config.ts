67b2112af152066a74d2291d9c54d3f9e8ea7d04
Rudra Patel <rudra.190410116095@gmail.com>
2025-02-11 14:25:22 -0500

    Auth pages and middleware

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Allows all origins
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" }, // Allowed HTTP methods
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }, // Allowed headers
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
