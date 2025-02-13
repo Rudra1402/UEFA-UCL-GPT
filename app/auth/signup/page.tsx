ad3c05f244074679816b5d26f5fd505ce7077d29
Rudra Patel <rudra.190410116095@gmail.com>
2025-02-12 23:13:48 -0500

    UI improvement

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,23 +1,15 @@
 "use client"
 
-import Link from "next/link"
+// import Link from "next/link"
 import AuthForm from "../../components/AuthForm"
 
 const SignupPage = () => {
     return (
         <div
-            className="h-full w-full p-3 bg-[#123456] !text-white text-center flex items-center justify-center"
+            className="h-full w-full p-3 bg-gray-800 !text-white text-center flex items-center justify-center"
         >
-            <div className="w-fit p-6 h-auto shadow-md rounded-md bg-white flex flex-col items-center justify-start">
+            <div className="w-fit h-auto shadow-md rounded-md flex flex-col items-center justify-start overflow-auto">
                 <AuthForm type={"signup"} />
-                <div
-                    className="text-gray-500 text-sm flex items-center gap-1 mt-2"
-                >Existing account!
-                    <Link
-                        href={'/auth/login'}
-                        className="text-blue-600"
-                    >Login</Link>
-                </div>
             </div>
         </div>
     )
